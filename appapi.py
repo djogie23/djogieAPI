c70dacca2a84747a76097df46f599a7877829047
djogie23 <73589924+djogie23@users.noreply.github.com>
2020-11-28 00:19:50 +0700

    Add files via upload

--- appapi.py
+++ appapi.py
@@ -2,56 +2,84 @@ from flask import Flask, request
 import pandas as pd 
 import sqlite3
 app = Flask(__name__) 
-conn = sqlite3.connect("data/chinook.db")
-
 
 #home root
 @app.route('/')
 def homepage():
-    return str(100*100) #"Hello world"
+    return "Welcome to djogie23 API!"
 
-# mendapatkan buku (static - no variable)
-@app.route('/ambil_buku')
-def ambilbuku():
-    data = pd.read_csv('data/books_c.csv')
-    return data.to_json()
+# Missing Value and Stack operation - static - Stock Closing Price 2018 in 365 days
+@app.route('/Stock2018')
+def stock():
+    stock = pd.read_pickle('data/stock')
+    closingprice = stock['Close']
+    year2018 = pd.date_range(start="2018-01-01", end="2018-12-31")
+    closingprice = closingprice.reindex(year2018)
+    closingprice = closingprice.ffill().bfill().stack()
+    return closingprice.to_json()
 
-@app.route('/ambil_pulsar')
-def ambilpulsar():
-    data = pd.read_csv('data/pulsar_stars.csv')
-    return data.to_json()
+# Groupby operation - static - Customer Spending
+@app.route('/customer_data')
+def customer():
+    conn = sqlite3.connect("data/chinook.db")
+    report = pd.read_sql_query("select Country, Total, FirstName, Lastname, InvoiceDate \
+                    from customers \
+                    left join invoices \
+                    on customers.CustomerId=invoices.CustomerId",conn)
+    report['Fullname'] = report['FirstName']+[' ']+report['LastName']
+    customerdata = report.groupby('Fullname').Total.sum().sort_values(ascending=False)
+    return customerdata.to_json()
 
-# buat endpoint yang menghasilkan hasil crosstabulasi 
-@app.route('/top_rating_book')
-def topratingbook ():
-    books = pd.read_csv('data/books_c.csv')
-    condition = books['average_rating'] == 5
-    books = books[condition]
-    return books.to_json()
+# Frequencies analysis (Crosstab) - static - Daily invoices by Country
+@app.route('/daily_invoices')
+def frequencies():
+    conn = sqlite3.connect("data/chinook.db")
+    report = pd.read_sql_query("select Country, Total, FirstName, Lastname, InvoiceDate \
+                    from customers \
+                    left join invoices \
+                    on customers.CustomerId=invoices.CustomerId",conn)
+    report['InvoiceDate'] = report['InvoiceDate'].astype('Datetime64')
+    report['InvoiceDOW'] = report['InvoiceDate'].dt.day_name()
+    report = pd.crosstab(
+    index=report['Country'],
+    columns=report['InvoiceDOW']
+    )
+    return report.to_json()
 
-@app.route('/get_author/<name>')
-def getauthor(name):
-    author = name
-    books = pd.read_csv('data/books_c.csv')
-    condition = books['authors'] == author
-    books = books[condition]
-    return books.to_json()
+# Date operation - dynamic - Total Daily Spending: Monday to Sunday
+@app.route('/daily_spending/<name>', methods=['GET'])
+def dateoperation(name):
+    InvoiceDOW = name
+    conn = sqlite3.connect("data/chinook.db")
+    report = pd.read_sql_query("select Country, Total, FirstName, Lastname, InvoiceDate \
+                    from customers \
+                    left join invoices \
+                    on customers.CustomerId=invoices.CustomerId",conn)
+    report['InvoiceDate'] = report['InvoiceDate'].astype('Datetime64')
+    report['InvoiceDOW'] = report['InvoiceDate'].dt.day_name()
+    condition = report['InvoiceDOW'] == InvoiceDOW
+    report = report[condition]
+    return report.to_json()
 
-# mendapatkan keseluruhan data dari <data_name> (dynamic - with variable)
-@app.route('/data/get/<data_name>', methods=['GET']) 
-def get_data(data_name): 
-    data = pd.read_csv('data/' + str(data_name))
-    return (data.to_json())
- 
+# Joining 4 table & Categorical Operation - dynamic
+@app.route('/music/<name>', methods=['GET'])
+def music(name):
+    Genre = name
+    conn = sqlite3.connect("data/chinook.db")
+    music = pd.read_sql_query(
+        '''
+        SELECT 
+        BillingCountry AS Country, genres.Name AS Genre
+        FROM invoices 
+        LEFT JOIN invoice_items ON invoices.InvoiceId = invoice_items.InvoiceId
+        LEFT JOIN tracks ON invoice_items.TrackId = tracks.TrackId 
+        LEFT JOIN genres ON tracks.GenreId = genres.GenreId
+        ''',conn)
+    music[['Country','Genre']] = music[['Country','Genre']].astype('category')
+    condition = music['Genre'] == Genre
+    music = music[condition]
+    return music.to_json()
 
-
-# mendapatkan data dengan filter nilai <value> pada kolom <column>
-@app.route('/data/get/equal/<data_name>/<column>/<value>', methods=['GET']) 
-def get_data_equal(data_name, column, value): 
-    data = pd.read_csv('data/' + str(data_name))
-    mask = data[column] == value
-    data = data[mask]
-    return (data.to_json())
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000) 
